28f9c873e8c9b47a2e0178ef831bd1b2d91d1689
Gabriel Bravo <gbravopa@nd.edu>
2020-12-17 10:36:37 -0500

    Implemented pytest for molecular-mass calculation

---
+++ molecool/tests/test_molecule.py
@@ -0,0 +1,16 @@
+"""
+Test for the molecule module
+"""
+
+# Import package, test suite, and other packages as needed
+import pytest
+import molecool
+
+def test_molecular_mass():
+    symbols = ['C', 'H', 'H', 'H', 'H']
+    
+    calculated_mass = molecool.calculate_molecular_mass(symbols)
+    
+    actual_mass = 16.04
+    
+    assert pytest.approx(actual_mass, abs=1e-2) == calculated_mass
